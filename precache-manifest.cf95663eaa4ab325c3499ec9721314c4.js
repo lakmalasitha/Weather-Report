298a4e0e24ce5449816ffa97a1bf198337d31f83
lakmalasitha <lakmalasitha52@gmail.com>
2020-05-29 15:27:44 +0530

    Updates

---
+++ precache-manifest.cf95663eaa4ab325c3499ec9721314c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa98616b9bea9561d04ce2adda908986",
+    "url": "/Weather-Report/index.html"
+  },
+  {
+    "revision": "ec04d386a55552552051",
+    "url": "/Weather-Report/static/css/main.ab8438d7.chunk.css"
+  },
+  {
+    "revision": "eb0181b683b1616bdb58",
+    "url": "/Weather-Report/static/js/2.ff0cef3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Report/static/js/2.ff0cef3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec04d386a55552552051",
+    "url": "/Weather-Report/static/js/main.d94aa2ee.chunk.js"
+  },
+  {
+    "revision": "653bb0989702d817401f",
+    "url": "/Weather-Report/static/js/runtime-main.3ee6ae10.js"
+  },
+  {
+    "revision": "e01a59d7c83f042d8600bab46958baf6",
+    "url": "/Weather-Report/static/media/black.e01a59d7.jfif"
+  }
+]);
